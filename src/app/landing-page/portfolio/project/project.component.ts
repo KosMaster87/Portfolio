fe657a871f9896df179513124803b420847a4466
Konstantin Aksenov <kav.pbg@gmail.com>
2024-11-03 13:24:33 -0300

    assets to global; interface Projects ok

--- src/app/landing-page/portfolio/project/project.component.ts
+++ src/app/landing-page/portfolio/project/project.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 
 import {
   Component,
@@ -6,43 +6,38 @@ import {
   ElementRef,
   ChangeDetectorRef,
   inject,
-} from "@angular/core";
+} from '@angular/core';
 
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from "@angular/animations";
-
-import { PortfolioDataService } from "./../../../shared/services/portfolioData/portfolio-data.service";
-import { TranslateModule } from "@ngx-translate/core";
+import { PortfolioDataService } from './../../../shared/services/portfolioData/portfolio-data.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { Project } from '../../../models/project/project';
 
 @Component({
-  selector: "app-project",
+  selector: 'app-project',
   standalone: true,
   imports: [CommonModule, TranslateModule],
-  templateUrl: "./project.component.html",
-  styleUrls: ["./project.component.scss"],
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.scss'],
 })
 export class ProjectComponent implements AfterViewInit {
   public isVisible = false;
   private el: ElementRef = inject(ElementRef);
   private cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
-  projectLength!: number;
   data = inject(PortfolioDataService);
+  projectsLength!: number;
+  projects!: Project[];
 
   ngOnInit(): void {
-    this.projectLength = this.data.myProjects.length;
+    this.projectsLength = this.data.myProjects.length;
+    this.projects = this.data.myProjects;
   }
 
   oddOrEven(num: number): string {
-    return num % 2 === 0 ? "even" : "odd";
+    return num % 2 === 0 ? 'even' : 'odd';
   }
 
   openLink(url: string) {
-    window.open(url, "_blank");
+    window.open(url, '_blank');
   }
 
   ngAfterViewInit() {
