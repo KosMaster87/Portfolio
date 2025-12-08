6f1a849d240b0f372507334042e9f41fb3bb12ee
Konstantin Aksenov <kav.pbg@gmail.com>
2025-12-08 18:19:39 -0300

    chore: remove old portfolio structure
    - Remove deprecated app.component files
    - Remove old homeprovide module structure
    - Remove old pages and models
    - Remove future-modul with old shared components
    - Clean up obsolete component implementations

--- src/app/future-modul/directives/icon-hover/icon-hover.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IconHoverDirective } from './icon-hover.directive';
-
-describe('IconHoverDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IconHoverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
